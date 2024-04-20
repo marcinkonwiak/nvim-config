26b30b42d43fddea6de900ed5fabc8585312d3d1
Marcin Konwiak <marcin2899@gmail.com>
2024-04-20 19:28:15 +0200

    Final changes? At least for now

--- lua/custom/plugins/editor.lua
+++ lua/custom/plugins/editor.lua
@@ -44,9 +44,8 @@ return {
     cmd = 'Neotree',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     },
     keys = {
       {
@@ -104,7 +103,7 @@ return {
       },
       default_component_configs = {
         indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+          with_expanders = true,
           expander_collapsed = '',
           expander_expanded = '',
           expander_highlight = 'NeoTreeExpander',
@@ -173,7 +172,7 @@ return {
     'echasnovski/mini.bufremove',
     keys = {
       {
-        '<leader>d',
+        '<leader>q',
         function()
           local bd = require('mini.bufremove').delete
           if vim.bo.modified then
@@ -191,7 +190,7 @@ return {
         desc = 'Delete Buffer',
       },
       -- stylua: ignore
-      { "<leader>D", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
+      { "<leader>Q", function() require("mini.bufremove").delete(0, true) end, desc = "Delete Buffer (Force)" },
     },
   },
 
--- lua/custom/plugins/utils.lua
+++ lua/custom/plugins/utils.lua
@@ -6,9 +6,9 @@ return {
     opts = { options = vim.opt.sessionoptions:get() },
     -- stylua: ignore
     keys = {
-      { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-      { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-      { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
+      { "<leader>yy", function() require("persistence").load() end, desc = "Restore Session" },
+      { "<leader>yl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>yd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
   },
 
@@ -20,19 +20,19 @@ return {
     event = 'VimEnter',
     opts = function()
       local logo = [[
-                                       ,--.           
-        ,--,--,  ,---.  ,---.,--.  ,--.`--',--,--,--. 
-        |      \| .-. :| .-. |\  `'  / ,--.|        | 
-        |  ||  |\   --.' '-' ' \    /  |  ||  |  |  | 
-        `--''--' `----' `---'   `--'   `--'`--`--`--' 
-                                                      
+           ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+           ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+           ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+           ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+           ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+           ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
       ]]
-
       logo = string.rep('\n', 3) .. logo .. '\n\n'
 
       local opts = {
         config = {
           header = vim.split(logo, '\n'),
+          footer = { '' },
         },
       }
 
